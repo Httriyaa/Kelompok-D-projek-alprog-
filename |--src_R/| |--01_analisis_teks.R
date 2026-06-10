63c61080fabe5bf219157bd7abf265c914149c76
Ovinorzk <novia.awaliyah@mhs.unsoed.ac.id>
2026-06-10 22:00:27 +0700

    Update | |--01_analisis_teks.R

--- |--src_R/| |--01_analisis_teks.R
+++ |--src_R/| |--01_analisis_teks.R
@@ -1,1 +1,13 @@
+#PROGRAM 1
+# Input teks dari pengguna
+teks <- readline(prompt = "Masukkan teks: ")
 
+# Menghitung jumlah kata
+jumlah_kata <- length(strsplit(teks, "\\s+")[[1]])
+
+# Menghitung jumlah kalimat
+jumlah_kalimat <- length(strsplit(teks, "\\.")[[1]])
+
+# Menampilkan hasil
+cat("\nTeks tersebut memuat", jumlah_kalimat,
+    "kalimat dan", jumlah_kata, "kata.\n")
