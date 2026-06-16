5a018903f3795179bc2abc17b0459b3c08458a6f
hanna072 <hanna.allyaardana@mhs.unsoed.ac.id>
2026-06-17 05:36:04 +0700

    Update | |-- 05_cluster_3d.R

--- |--src_R/| |-- 05_cluster_3d.R
+++ |--src_R/| |-- 05_cluster_3d.R
@@ -1,80 +1,37 @@
-#Untuk titik (9,7,5)
-# Input titik U
-u1 <- 9 
-u2 <- 7
-u3 <- 5
-
-# Hitung jarak ke Cluster A (2, 1, 3)
-jarakA <- sqrt((u1 - 2)^2 + (u2 - 1)^2 + (u3 - 3)^2)
-# Hitung jarak ke Cluster B (1, -4, 6)
-jarakB <- sqrt((u1 - 1)^2 + (u2 + 4)^2 + (u3 - 6)^2)
-# Hitung jarak ke Cluster C (-2, 3, -2)
-jarakC <- sqrt((u1 + 2)^2 + (u2 - 3)^2 + (u3 + 2)^2)
-
-# Menampilkan jarak
-print("Jarak ke Cluster A =", jarakA)
-print("Jarak ke Cluster B =", jarakB)
-print("Jarak ke Cluster C =", jarakC)
-
-# Menentukan cluster
-if (jarakA < jarakB && jarakA < jarakC) {
-  print("Titik U termasuk Cluster A")
-} else if (jarakB < jarakA && jarakB < jarakC) {
-  print("Titik U termasuk Cluster B")
-} else {
-  print("Titik U termasuk Cluster C")
+# Membuat fungsi untuk menentukan cluster
+cluster <- function(u1, u2, u3) {
+  
+  # Menghitung jarak ke masing-masing cluster
+  jarakA <- sqrt((u1 - 2)^2 + (u2 - 1)^2 + (u3 - 3)^2)
+  jarakB <- sqrt((u1 - 1)^2 + (u2 + 4)^2 + (u3 - 6)^2)
+  jarakC <- sqrt((u1 + 2)^2 + (u2 - 3)^2 + (u3 + 2)^2)
+  
+  # Menampilkan jarak
+  print(paste("Jarak ke Cluster A =", jarakA))
+  print(paste("Jarak ke Cluster B =", jarakB))
+  print(paste("Jarak ke Cluster C =", jarakC))
+  
+  # Menentukan cluster
+  if (jarakA < jarakB && jarakA < jarakC) {
+    return("Titik U termasuk Cluster A")
+  } else if (jarakB < jarakA && jarakB < jarakC) {
+    return("Titik U termasuk Cluster B")
+  } else {
+    return("Titik U termasuk Cluster C")
+  }
 }
 
-#Untuk titik (1,-4,6)
-# Input titik U
-u4 <- 2
-u5 <- -3
-u6 <- 5
+# Memanggil fungsi dengan titik (9, 7, 5)
+hasil_1 <- cluster(9, 7, 5)
+# Memanggil fungsi dengan titik (2, -3, 5)
+hasil_2 <- cluster(2, -3, 5)
+# Memanggil fungsi dengan titik (-1, 2, -3)
+hasil_3 <- cluster(-1, 2, -3)
+# Memanggil fungsi dengan titik (9, 7, 5)
+hasil_4 <- cluster("a", "b", "c")
 
-# Hitung jarak ke Cluster A (2, 1, 3)
-jarakA <- sqrt((u4 - 2)^2 + (u5 - 1)^2 + (u6 - 3)^2)
-# Hitung jarak ke Cluster B (1, -4, 6)
-jarakB <- sqrt((u4 - 1)^2 + (u5 + 4)^2 + (u6 - 6)^2)
-# Hitung jarak ke Cluster C (-2, 3, -2)
-jarakC <- sqrt((u4 + 2)^2 + (u5 - 3)^2 + (u6 + 2)^2)
-
-# Menampilkan jarak
-print("Jarak ke Cluster A =", jarakA)
-print("Jarak ke Cluster B =", jarakB)
-print("Jarak ke Cluster C =", jarakC)
-
-# Menentukan cluster
-if (jarakA < jarakB && jarakA < jarakC) {
-  print("Titik U termasuk Cluster A")
-} else if (jarakB < jarakA && jarakB < jarakC) {
-  print("Titik U termasuk Cluster B")
-} else {
-  print("Titik U termasuk Cluster C")
-}
-
-#Untuk titik (-1,2,-3)
-# Input titik U
-u7 <- -1
-u8 <- 2
-u9 <- -3
-
-# Hitung jarak ke Cluster A (2, 1, 3)
-jarakA <- sqrt((u7 - 2)^2 + (u8 - 1)^2 + (u9 - 3)^2)
-# Hitung jarak ke Cluster B (1, -4, 6)
-jarakB <- sqrt((u7 - 1)^2 + (u8 + 4)^2 + (u9 - 6)^2)
-# Hitung jarak ke Cluster C (-2, 3, -2)
-jarakC <- sqrt((u7 + 2)^2 + (u8 - 3)^2 + (u9 + 2)^2)
-
-# Menampilkan jarak
-print("Jarak ke Cluster A =", jarakA)
-print("Jarak ke Cluster B =", jarakB)
-print("Jarak ke Cluster C =", jarakC)
-
-# Menentukan cluster
-if (jarakA < jarakB && jarakA < jarakC) {
-  print("Titik U termasuk Cluster A")
-} else if (jarakB < jarakA && jarakB < jarakC) {
-  print("Titik U termasuk Cluster B")
-} else {
-  print("Titik U termasuk Cluster C")
-}
+# Menampilkan hasil
+print(hasil_1)
+print(hasil_2)
+print(hasil_3)
+print(hasil_4)
