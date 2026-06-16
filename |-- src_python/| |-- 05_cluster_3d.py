9a760281d912d13553456e8de6e3d4e77d9166ce
hanna072 <hanna.allyaardana@mhs.unsoed.ac.id>
2026-06-17 05:03:33 +0700

    Update | |-- 05_cluster_3d.py

--- |-- src_python/| |-- 05_cluster_3d.py
+++ |-- src_python/| |-- 05_cluster_3d.py
@@ -63,3 +63,26 @@ elif jarakB < jarakA and jarakB < jarakC:
     print("Titik U termasuk Cluster B")
 else:
     print("Titik U termasuk Cluster C")
+
+
+import math
+#Terhadap titik (a,b,c)
+# Input titik U
+u1 = a
+u2 = b
+u3 = c
+
+# Hitung jarak ke Cluster A (2, 1, 3)
+jarakA = math.sqrt((u1 - 2)**2 + (u2 - 1)**2+ (u3 - 3)**2)
+# Hitung jarak ke Cluster B (1, -4, 6)
+jarakB = math.sqrt((u1 - 1)**2 + (u2 + 4)**2 + (u3 - 6)**2)
+# Hitung jarak ke Cluster C (-2, 3, -2)
+jarakC = math.sqrt((u1 + 2)**2 + (u2 - 3)**2 + (u3 + 2)**2)
+
+# Menentukan cluster
+if jarakA < jarakB and jarakA < jarakC:
+    print("Titik U termasuk Cluster A")
+elif jarakB < jarakA and jarakB < jarakC:
+    print("Titik U termasuk Cluster B")
+else:
+    print("Titik U termasuk Cluster C")
