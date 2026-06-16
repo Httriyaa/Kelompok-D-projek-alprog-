64f002db5efa23e34c2063290e096d5fd5f6cbc7
Aya! <thaa.naurah08@gmail.com>
2026-06-16 12:23:04 +0700

    Add files via upload

---
+++ |--src_R/Program 4 nip_asn.R
@@ -0,0 +1,126 @@
+# Input NIP
+nip <- readline("Masukkan NIP:")
+
+# Ambil bagian tanggal lahir
+year <- substr(nip, 1, 4)
+month <- substr(nip, 5, 6)
+date <- substr(nip, 7, 8)
+
+# Menentukan nama bulan
+if (month == "01") {
+  name_of_month <- "Januari"
+} else if (month == "02") {
+  name_of_month <- "Februari"
+} else if (month == "03") {
+  name_of_month <- "Maret"
+} else if (month == "04") {
+  name_of_month <- "April"
+} else if (month == "05") {
+  name_of_month <- "Mei"
+} else if (month == "06") {
+  name_of_month <- "Juni"
+} else if (month == "07") {
+  name_of_month <- "Juli"
+} else if (month == "08") {
+  name_of_month <- "Agustus"
+} else if (month == "09") {
+  name_of_month <- "September"
+} else if (month == "10") {
+  name_of_month <- "Oktober"
+} else if (month == "11") {
+  name_of_month <- "November"
+} else if (month == "12") {
+  name_of_month <- "Desember"
+} else {
+  name_of_month <- "Bulan tidak valid"
+}
+
+# Output
+cat("Tanggal lahir ASN:\n")
+cat(date, name_of_month, year)
+
+
+
+# Input NIP
+nip <- readline("Masukkan NIP:")
+
+# Ambil bagian tanggal lahir
+year <- substr(nip, 1, 4)
+month <- substr(nip, 5, 6)
+date <- substr(nip, 7, 8)
+
+# Menentukan nama bulan
+if (month == "01") {
+  name_of_month <- "Januari"
+} else if (month == "02") {
+  name_of_month <- "Februari"
+} else if (month == "03") {
+  name_of_month <- "Maret"
+} else if (month == "04") {
+  name_of_month <- "April"
+} else if (month == "05") {
+  name_of_month <- "Mei"
+} else if (month == "06") {
+  name_of_month <- "Juni"
+} else if (month == "07") {
+  name_of_month <- "Juli"
+} else if (month == "08") {
+  name_of_month <- "Agustus"
+} else if (month == "09") {
+  name_of_month <- "September"
+} else if (month == "10") {
+  name_of_month <- "Oktober"
+} else if (month == "11") {
+  name_of_month <- "November"
+} else if (month == "12") {
+  name_of_month <- "Desember"
+} else {
+  name_of_month <- "Bulan tidak valid"
+}
+
+# Output
+cat("Tanggal lahir ASN:\n")
+cat(date, name_of_month, year)
+
+
+
+# Input NIP
+nip <- readline("Masukkan NIP:")
+
+# Ambil bagian tanggal lahir
+year <- substr(nip, 1, 4)
+month <- substr(nip, 5, 6)
+date <- substr(nip, 7, 8)
+
+# Menentukan nama bulan
+if (month == "01") {
+  name_of_month <- "Januari"
+} else if (month == "02") {
+  name_of_month <- "Februari"
+} else if (month == "03") {
+  name_of_month <- "Maret"
+} else if (month == "04") {
+  name_of_month <- "April"
+} else if (month == "05") {
+  name_of_month <- "Mei"
+} else if (month == "06") {
+  name_of_month <- "Juni"
+} else if (month == "07") {
+  name_of_month <- "Juli"
+} else if (month == "08") {
+  name_of_month <- "Agustus"
+} else if (month == "09") {
+  name_of_month <- "September"
+} else if (month == "10") {
+  name_of_month <- "Oktober"
+} else if (month == "11") {
+  name_of_month <- "November"
+} else if (month == "12") {
+  name_of_month <- "Desember"
+} else {
+  name_of_month <- "Bulan tidak valid"
+}
+
+# Output
+cat("Tanggal lahir ASN:\n")
+cat(date, name_of_month, year)
